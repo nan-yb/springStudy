08d459d4024361ecc72aa1a6bbbac051bce8884c
yb <dydqls5757@gmail.com>
2022-10-03 13:29:58 +0900

    implement JavaSort

---
+++ app/src/test/java/logic/JavaSortTest.java
@@ -0,0 +1,26 @@
+package logic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JavaSortTest {
+
+
+  @DisplayName("자바소트 - 리스트를 넣으면 정렬된 결과를 보여준다")
+  @Test
+  void given_List_WhenExecuting_ThenReturnSortedList() {
+      // Given
+      JavaSort<Integer> javaSort = new JavaSort<>();
+
+
+      // When
+      List<Integer> actual = javaSort.sort(List.of(3, 2, 4, 5, 1));
+
+      // Then
+      assertEquals(List.of(1, 2, 3, 4, 5), actual);
+  }
+}
