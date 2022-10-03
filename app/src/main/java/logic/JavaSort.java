72f7aea7b765cfdd2e00d3fb7886f9dd829bb7b4
nan-yb <78857814+nan-yb@users.noreply.github.com>
2022-10-03 14:09:32 +0900

    Revert "Merge pull request #5 from nan-yb/feature/#4-enhance"
    This reverts commit 2bc6d50532a87fd82b1188f9eb368c49020707cb.

--- app/src/main/java/logic/JavaSort.java
+++ app/src/main/java/logic/JavaSort.java
@@ -1,12 +1,11 @@
-package study.logic;
+package logic;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class JavaSort<T extends Comparable<T>> implements Sort<T>{
+public class JavaSort<T extends Comparable<T>> {
   
-  @Override
   public List<T> sort(List<T> list){
     List<T> output = new ArrayList<>(list);
     
--- app/src/test/java/logic/JavaSortTest.java
+++ app/src/test/java/logic/JavaSortTest.java
@@ -1,4 +1,4 @@
-package study.logic;
+package logic;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 
 public class JavaSortTest {
 
